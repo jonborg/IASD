d9e07f2ca4651df9288e1f9e55fe945276f211ab
jonborg <joaomanborges@hotmail.com>
2016-10-13 23:41:40 +0100

    added stack read in file

--- proj1.py
+++ proj1.py
@@ -1,53 +1,40 @@
 import sys
 
-class cask:
-    def __init__(self,id,l=1,w=0):
-        self.id=id
-        self.l=l
-        self.w=w
-
-class stack:
-    def __init__(self,id,s):
-        self.id=id
-        self.s=s
-        self.casks=[] #list- use append to insert and pop to remove (LIFO)
-        self.adjacent={}
-
 class graph:
     def __init__(self):
-        self.n_casks=0
-        self.n_nodes=0
-
-        self.casks={}
-        self.nodes={}
+        self.casks={} #casks characteristics
+        self.stack={} #stack has what cask?
+        self.nodes={} #nodes connections
 
     def add_node(self,node):
         self.nodes.append(node)
-        self.n_nodes=self.n_nodes+1
         
     def add_cask(self,id,l,w):
         self.casks[id]=[l,w]
-        self.n_casks=self.n_casks+1
-
+        
+    def add_stack(self,id,w,c=[]):
+        self.stack[id]=[w,c]
 
 G=graph()
 
-print(sys.argv[1])
 file=open(sys.argv[1])
 lines=file.readlines()
-print (lines)
 
 for x in lines:
-    
     w=x.split()
-    print (w)
+    print(w)
     i=list(x)
-    print(i[0])
     if i[0]=="C": #cask
         G.add_cask(w[0],w[1],w[2])
     if i[0]=="S": #stack
-        
+        if len(w)>2:
+            G.add_stack(w[0],w[1],w[2:len(w)])
+        else:
+            G.add_stack(w[0],w[1])            
+
+print()     
 print(G.casks)
+print(G.stack)
         
         
         
