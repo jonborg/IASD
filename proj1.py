ef29a8f9cd8cdab799545005c9518ce16536fe5d
jonborg <joaomanborges@hotmail.com>
2016-10-18 23:11:30 +0100

    first implementations of general algorithm

--- proj1.py
+++ proj1.py
@@ -9,8 +9,8 @@ class graph:
     def add_cask(self,id,l,w):
         self.cask[id]=[l,w]
         
-    def add_stack(self,id,w,c=[]):
-        self.stack[id]=[w,c]
+    def add_stack(self,id,S,s,c=[]):
+        self.stack[id]=[S,s,c]
 
     def add_node(self,node,neighbour,cost):
         self.node.update({node:{neighbour:float(cost)}})
@@ -33,7 +33,10 @@ def open_file(file):
             G.add_cask(w[0],w[1],w[2])
         if i[0]=="S": #stack
             if len(w)>2:
-                G.add_stack(w[0],w[1],w[2:len(w)])
+                occupied=0
+                print(w[3:len(w)])
+                casks=w[3:len(w)]
+                G.add_stack(w[0],w[1],occupied,casks)
             else:
                 G.add_stack(w[0],w[1])
         if i[0]=="E": #edge
@@ -53,12 +56,12 @@ def open_file(file):
 class state_node:
     def __init__(self,setup):
         self.state_space=setup[0] #[position,Cask,nº of loads]
-        self.parent=setup[1]
-        self.children=setup[2]
-        self.depth=setup[3]
-        self.gx=setup[4]
+        self.parent=setup[1] #previous state space
+        self.children=setup[2] #next state spaces
+        self.depth=setup[3] #depth level
+        self.gx=setup[4] #total cost until current node
         self.last_op=setup[5]    #[op,arg1,arg2,cost]
-
+    
     def show(self):
         print()
         print("-------------------------")
@@ -76,36 +79,56 @@ class state_node:
         print(self.last_op)
         print("-------------------------")
         print()
-def move(snode,dest,cost):
-    setup=[[dest,snode.state_space[1],snode.state_space[2]],snode.state_space,[],snode.depth+1,snode.gx+cost,["move",snode.state_space[0],dest,cost]]
-    new=state_node(setup)
-    snode.children=[new.state_space]
-    return new
+
     
+    def find_children(self,stacks,nodes,open_list):
+        if self.state_space[0] in stacks:
+            print(1)
+            
+def un_load(G, current):
+    if current.state_space[0] is "":
+        load=G.stack[current.state_space[0]].pop()
         
-
-G=open_file(sys.argv[1])
-print()     
-print(G.cask)
-print()     
-print(G.stack)
-print()     
-print(G.node)
-
-ini_list=[state_node([["EXIT","",0],[],[],0,0,[]])]
-
-if G.node["EXIT"]["C"]:
-    current=move(ini_list[0],"C",G.node["EXIT"]["C"])
-ini_list.append(current)
-
-if G.node["C"]["B"]:
-    current=move(ini_list[1],"B",G.node["C"]["B"])
-ini_list.append(current)
-
-ini_list[0].show()
-ini_list[1].show()
-ini_list[2].show()
-
-
+        ss=[current.state_space[0],load,current.state_space[2]+1]
+        setup=[ss,current.state_space,[],current.depth+1,current.gx+1+G.cask[load][1],["LOAD",load,current.state_space[0],1+G.stack[load][1]]]
+        state_node(new,setup)
+    else:
         
+        load=current.state_space[1]
+        if G.stack[current.state_space[0]][0]
+            G.stack[current.state_space[0]].append(load)
+            ss=[current.state_space[0],"",current.state_space[2]+1]
+            setup=[ss,current.state_space,[],current.depth+1,current.gx+1+G.cask[load][1],["UNLOAD",load,current.state_space[0],1+G.stack[load][1]]]
+            state_node(new,setup)
         
+def move(current,dest,cost):
+    setup=[[dest,current.state_space[1],current.state_space[2]],current.state_space,[],current.depth+1,current.gx+cost,["move",current.state_space[0],dest,cost]]
+    new=state_node(setup)
+    current.children=[new.state_space]
+    return new
+
+                                                                                        
+    
+def main():
+    G=open_file(sys.argv[1])
+    print()     
+    print(G.cask)
+    print()     
+    print(G.stack)
+    print()     
+    print(G.node)
+
+
+    open_list=[state_node([["EXIT","",0],[],[],0,0,[]])]
+    closed_list=[]
+    while 1:
+        if len(open_list)==0:
+            print ("FAILURE")
+            return
+        current=open_list.pop()
+        if current.state_space[0:2] is ["EXIT",sys.argv[2]]:
+            print (current.state_space)
+            return
+        closed_list.append(current)
+if __name__ == "__main__":
+    main()
