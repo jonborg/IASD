12ed25fbdaf827318596ac02443e9a4c9e4e178d
Helder <heldertz2@gmail.com>
2016-10-27 18:01:20 +0100

    uninformed2.py now working

--- uninformed2_backup.py
+++ uninformed2_backup.py
@@ -1,43 +1,35 @@
-
-
 import sys
 import copy
 
-import uninformed
-
 class graph:
     def __init__(self):
-        self.cask  = {} # list of casks, a cask has length and weight
-        self.stack = {} # list of stacks, a stack has size and casks
-        self.node  = {} # list of node conenctions, a node has neighbours and costs
+        self.cask={} #casks characteristics
+        self.stack={} #stack has what cask?
+        self.node={} #nodes connections
         
-    def add_cask(self, id, l, w):
-        self.cask[id] = [float(l), float(w)]
+    def add_cask(self,id,l,w):
+        self.cask[id]=[float(l),float(w)]
         
-    def add_stack(self, id, S, c=[]):
-        self.stack[id] = [float(S), c]
+    def add_stack(self,id,S,c=[]):
+        self.stack[id]=[float(S),c]
 
-    def add_node(self, node, neighbour, cost):
-        self.node.update( {node:{neighbour:float(cost)}} )
+    def add_node(self,node,neighbour,cost):
+        self.node.update({node:{neighbour:float(cost)}})
         
-    def add_connection(self, node, neighbour, cost):
-        self.node[node].update( {neighbour:float(cost)} )
-    
+    def add_connection(self,node,neighbour,cost):
+        self.node[node].update({neighbour:float(cost)})
 
 
 class state_node:
     def __init__(self,setup):
-        self.state_space = setup[0]   # [position, Cask, nº of loads]
-        self.parent   = setup[1]      # previous state space
-        self.children = setup[2]      # next state spaces
-        self.depth    = setup[3]      # depth level
-        self.gx       = setup[4]      # total cost until current node
-        self.last_op  = setup[5]      # [op, arg1, arg2, cost]
-        self.stack_status=setup[6] #{Stack: occupied space}
-        
-#    def has_cask(self):
-#        """checks if robot has a cask in current node"""
-#        return (self.state_space[1]=="")
+        self.state_space=setup[0] #[position,Cask,nº of loads]
+        self.parent=setup[1] #previous state space
+        self.children=setup[2] #next state spaces
+        self.depth=float(setup[3]) #depth level
+        self.gx=float(setup[4]) #total cost until current node
+        self.hx=float(setup[5])
+        self.last_op=setup[6]    #[op,arg1,arg2,cost]
+        self.stack_status=setup[7] #{Stack: occupied space}
     
     def show(self):
         print()
@@ -52,6 +44,8 @@ class state_node:
         print(self.depth)
         print("Path Cost: ",end="")
         print(self.gx)
+        print("Estimated Cost to go: ",end="")
+        print(self.hx)
         print("Last Operation: ",end="")
         print(self.last_op)
         print("-------------------------")
@@ -66,9 +60,9 @@ class state_node:
 
 
 
+
 def open_file(file):
-    """reads input file and creates graph of the problem"""
-    
+
     G=graph()
     
     f=open(file)
@@ -104,11 +98,7 @@ def open_file(file):
 
 
         
-def unload(G, current, open_list, closed_list):
-    """load cask to robot or unload cask to a given stack
-    unable to unload on a stack without room for the cask
-    unable to load from an empty stack"""
-    
+def un_load(G, current,open_list, closed_list):
     if current.state_space[1] is "": #No cask in robot-LOAD
         if current.last_op[0] != "unload":
             if current.stack_status[current.state_space[0]][1]==[]: #Se não tiver casks no stack
@@ -123,7 +113,11 @@ def unload(G, current, open_list, closed_list):
                 if any (node.state_space==ss for node in (open_list+closed_list)):               
                     return []
                 else:
-                    setup=[ss,current.state_space,[],current.depth+1,current.gx+1+G.cask[load][1],["load",load,current.state_space[0],1+G.cask[load][1]],new_status]
+                    if current.state_space[0]==stack_loc:
+                        hcost=current.hx-(1+min_w)
+                    else:
+                        hcost=current.hx+(1+min_w)
+                    setup=[ss,current.state_space,[],current.depth+1,current.gx+1+G.cask[load][1],hcost,["load",load,current.state_space[0],1+G.cask[load][1]],new_status]
                     new=state_node(setup)
                     return new
         else:
@@ -146,7 +140,8 @@ def unload(G, current, open_list, closed_list):
                 if any (node.state_space==ss for node in (open_list + closed_list)):
                     return[]
                 else:
-                    setup=[ss,current.state_space,[],current.depth+1,current.gx+1+G.cask[load][1],["unload",load,current.state_space[0],1+G.cask[load][1]],new_status]
+                    hcost=current.hx-(1+min_w)
+                    setup=[ss,current.state_space,[],current.depth+1,current.gx+1+G.cask[load][1],hcost,["unload",load,current.state_space[0],1+G.cask[load][1]],new_status]
                     new=state_node(setup)
                     return new
             else:
@@ -155,24 +150,15 @@ def unload(G, current, open_list, closed_list):
             return[]
         
         
-def move(G, current, dest, cost, open_list):
-    """move robot from current location to adjacent node"""
+def move(G,current,dest,cost,open_list):
     
-    # get total cost
-    if current.state_space[1] == "":
-        tcost = cost
+    if current.state_space[1]=="":
+        gcost=cost
     else:
-        tcost = (1+G.cask[current.state_space[1]][1])*cost
-
-    # build new setup for the children
-    #   [position, Cask, nº of loads]
-    #   previous state space
-    #   next state spaces
-    #   depth level
-    #   total cost until current node
-    #   [op, arg1, arg2, cost]
+        gcost=(1+G.cask[current.state_space[1]][1])*cost
+    hcost=current.hx
     print("MOVE" ,current.stack_status)
-    setup=[[dest,current.state_space[1],current.state_space[2]],current.state_space,[],current.depth+1,current.gx+tcost,["move",current.state_space[0],dest,tcost],current.stack_status]
+    setup=[[dest,current.state_space[1],current.state_space[2]],current.state_space,[],current.depth+1,current.gx+gcost,hcost,["move",current.state_space[0],dest,gcost,hcost],current.stack_status]
     new=state_node(setup)
     if any (node.state_space==new.state_space for node in open_list):
         for i,j in enumerate(open_list):
@@ -186,87 +172,77 @@ def move(G, current, dest, cost, open_list):
     else:
         current.children=[new.state_space]
         open_list.append(new)
-    
-    
-    
-
-def belong_to_open_list(open_list, closed_list, child):
-    """Checks if child exists in open_list"""
-    
-    if child in open_list:
-        return True
-    else:
-        return False
-        
-        
-        
-
-def find_children(G, current, open_list, closed_list):
-    """Appends children from current node to open_list, 
-    as per the general search algorithm"""
-    
-    children = []
-
-    # to expand the children, we need to see if we are in a stack node
-    # if so,  then we can either load or unload (or move)
-    # the unload function will give us all the children that result
-    # in either a load action or unload action
-    if current.state_space[0] in G.stack.keys():    # current node is a stack
-        children = unload(G, current, open_list, closed_list)
-        if children == []:
+            
+                                                                                            
+def find_children(G,current,open_list,closed_list):
+    children=[]
+    if current.state_space[0] in G.stack.keys():
+        children=un_load(G,current,open_list,closed_list)
+        if children==[]:
             pass
         else:
-            # need to send child to closed_list if duplicate already exists in 
-            # open_list
-            if not belong_to_open_list(open_list, closed_list, children):
-                open_list.append(children)
-            else:
-                closed_list.append(children)
-    
-    # iterate through the neighbours of current node to see if
-    # any children result from the move action
+            open_list.append(children)
+            
     for neighbour in G.node[current.state_space[0]]:
-        if any (node.state_space == [str(neighbour), current.state_space[1], current.state_space[2]] for node in (open_list + closed_list)):
+        if any (node.state_space==[str(neighbour),current.state_space[1],current.state_space[2]] for node in closed_list):
             pass
         else:
-            move(G, current, str(neighbour), G.node[current.state_space[0]][str(neighbour)], open_list)
-            # need to send child to closed_list if duplicate already exists in 
-            # open_list
-#            if not belong_to_open_list(open_list, closed_list, children):
-#                open_list.append(children)
-#            else:
-#                closed_list.append(children)
-        
+            move(G,current,str(neighbour),G.node[current.state_space[0]][str(neighbour)],open_list)
+            
     return open_list
 
 
-
 def print_output(final,closed_list):
-    
-    current = final
-    commands = []
-    
-    while current.parent != []:
+    current=final
+    commands=[]
+    total=0
+    while current.parent !=[]:
         commands.append(current.last_op)
         for index, item in enumerate(closed_list):
             if item.state_space == current.parent:
                 break
-        current = closed_list[index]
+        current=closed_list[index]
 
-    f = open('results.txt','wt')
-    while commands != []:
-        line = commands.pop()
-        print(*line, sep=' ', file=f)
-    print(final.gx, file=f)
-    f.close()
+    output_f=sys.argv[1].replace(".dat","")
+    print(output_f)
+    output_f=output_f+"_"+sys.argv[2]+".out"
+    sys.stdout=open(output_f,'w')
+    while commands!=[]:
+        line=commands.pop()
+        print(line[0],line[1],line[2],line[3])
+    print(final.gx)
 
+
+
+
+
+
+def hx_parameters(G):
+    global ind
+    global n_cask_objective
+    global min_w
+    global stack_loc
+    min_w=-1
+
+    for stack in G.stack.keys(): # for future heuristic functions that may need the location of tha goal cask in the stack
+        casks=G.stack[stack][1]
+        for cask in casks:
+            if cask==sys.argv[2]:
+                ind=G.stack[stack][1].index(sys.argv[2])
+                n_cask_objective=len(casks)
+                stack_loc=stack
+    for cask in G.cask.keys():
+        if G.cask[cask][1]<min_w:
+            min_w=G.cask[cask][1]
+            continue
+        if min_w==-1:
+            min_w=G.cask[cask][1]
+    
 
 
 
     
 def main():
-    
-    # create graph of the problem
     G=open_file(sys.argv[1])
     print()     
     print(G.cask)
@@ -275,6 +251,7 @@ def main():
     print()     
     print(G.node)
     
+    hx_parameters(G)
     init_stack_occupied={}
     for stack in G.stack.keys():
         init_stack_occupied[stack]=[0, []]
@@ -282,48 +259,46 @@ def main():
             
             init_stack_occupied[stack][0]=init_stack_occupied[stack][0]+G.cask[cask][0]
             init_stack_occupied[stack][1].append(cask)
-
-    open_list = [state_node([["EXIT","",0],[],[],0,0,[], init_stack_occupied])]    # the CTS starts at the EXIT node, with no casks
-    closed_list = []
-
+   
+    open_list=[state_node([["EXIT","",0],[],[],0,0,(1+min_w)*(2*(n_cask_objective-ind)-1),[],init_stack_occupied])]
+    closed_list=[]
     while 1:
-        
-        #time.sleep(5)
-        if len(open_list) == 0:
+        #a=input("waiting: ")
+        if len(open_list)==0:
             print()
             print("FAILURE")
             return
-            
-        #current = open_list.pop()
-        current = uninformed.choose_next_node(open_list, closed_list)
+        current=open_list.pop()
         print()
         print (current.state_space)
-        print("Action: " + str(current.last_op))
-        
-        if current.state_space[0:2] == ["EXIT",sys.argv[2]]:    # args: 'python', 'general.py', 's1.dat', 'Cb'
+        if current.state_space[0:2] == ["EXIT",sys.argv[2]]:
             print()
             print("FINISH!!!")
             print(current.state_space)
-            print_output(current, closed_list)
+            print("Number of nodes opened: ", end="")
+            print(len(closed_list))
+            print_output(current,closed_list)
             return
-        
-
         closed_list.append(current)
+        
+        open_list=find_children(G,current,open_list,closed_list)
+        open_list.sort(key=lambda node: node.gx+node.hx,reverse=True)
+        
         print()
         print("Open List:")
         print("----------------------")      
         for node in open_list:
-            print(node.state_space)    
+            print("{} <-- {} h= {}".format(node.state_space,node.parent,node.stack_status))    
         print("----------------------")
         print()
         print("Closed List:")
         print("----------------------")      
         for node in closed_list:
-            print(node.state_space)    
+            print("{} <-- {} h= {}".format(node.state_space,node.parent,node.stack_status))    
         print("----------------------")
         
-        open_list=find_children(G,current,open_list,closed_list)
-        
+        for node in open_list:
+            print(node.gx+node.hx)        
         
 if __name__ == "__main__":
     main()
